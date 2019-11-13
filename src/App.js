bdb95a24cda39edc825b5904e0e327342190b432
sulav_21 <thapasulup@gmail.com>
2019-11-13 17:12:02 +0545

    Modified the app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Table from './Table'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {name: "Sulav"}
+  }
+  changeName = () => {
+    this.setState({name: "Silva"});
+  }
+  render(){
+    return(
+      <div className="Container">
+        <h1>Shopping list for {this.state.name}</h1>
+        <ul>
+          <li>Books</li>
+          <li>Arduino Devices</li>
+          <li>Love</li>
+        </ul>
+
+        <button
+       type="button"
+       onClick={this.changeName}
+      >
+        Change Name
+      </button>
+      </div>
+    )
+  }
 }
 
-export default App;
+class New extends React.Component{
+  render(){
+    return(
+      <div className="hello">
+      <h1>Hello World</h1>
+      <App/>
+      </div>
+    )
+  }
+}
+
+export default New;
