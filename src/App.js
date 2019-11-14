4f5ffd9d1e5ca2067e66c56aaeb65d93003c512c
sulav_21 <thapasulup@gmail.com>
2019-11-14 17:48:49 +0545

    Used Bootstrap for styling the button and form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import Table from './Table';
 import './App.css'
+import {Button} from 'react-bootstrap';
+import {Form} from 'react-bootstrap';
+import {Card} from 'react-bootstrap';
+
 
 
 class App extends React.Component{
@@ -20,13 +24,16 @@ class App extends React.Component{
           <li>Arduino Devices</li>
           <li>Love</li>
         </ul>
-
-        <button
+        <Button variant="primary">Primary</Button>
+        <br/>
+        <br/>
+        <Button variant="info"
        type="button"
        onClick={this.changeName}
       >
         Change Name
-      </button>
+      </Button>
+      
       </div>
     )
   }
@@ -242,7 +249,6 @@ class ErrorForm extends React.Component {
       <textarea></textarea>
       <br/>
       <br/>
-
       <select>
         <option value="instagram">Instagram</option>
         <option value="facebook">Facebook</option>
@@ -254,4 +260,26 @@ class ErrorForm extends React.Component {
   }
 }
 
-export default ErrorForm;
+class TestDesign extends React.Component{
+  render(){
+    return(
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>UserName</Form.Label>
+          <Form.Control type="text" placeholder="Enter UserName" />
+          <Form.Text className="text-muted">
+         </Form.Text>
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    );
+  }
+}
+
+export default TestDesign;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component }from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import "bootstrap/dist/css/bootstrap.css";
 import Car from './App'
 
 ReactDOM.render(<Car/>, document.getElementById('root'))
