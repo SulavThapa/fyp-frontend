1b310c628fb96e94690998fab9e487c363f1f146
sulav_21 <thapasulup@gmail.com>
2019-12-15 13:37:03 +0545

    Table is added and button are also added.

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,39 +1,50 @@
-import React, { Component } from 'react'
-import NavBar from '../Navbar'
-import Table from 'react-bootstrap/Table'
-import { Card, Button } from 'react-bootstrap'
+import React, { Component } from 'react';
+import NavBar from '../Navbar';
+import Table from 'react-bootstrap/Table';
+import { Card, Button } from 'react-bootstrap';
+// import Fetchdata from '../FetchData/Fetch';
 
 const NewAdmin = () => (
     <div>
         <NavBar />
         <br></br>
-        <Card className='mx-auto' bg="light" text="white" style={{ width: '80rem', height: '100%', padding: 25 }}>
-            <Button variant="success" style={{ width: '1in',position: 'absolute', right: '2rem', top:'3%' }}>Add</Button>
+        <Card className='mx-auto' bg="light" text="white" style={{ width: '80%', height: '100%', padding: 25 }}>
+            <h3 style={{color:'black', top:'7%',position: 'absolute', left: '2rem'}}>Class 10 Students data</h3>
+            <Button variant="success" style={{ width: '1in', position: 'absolute', right: '2rem', top: '3%' }}>Add</Button>
+            <Button variant="primary" style={{ width: '1in', position: 'absolute', right: '8.5rem', top: '3%' }}>Import</Button>
+            <Button variant="danger" style={{ width: '1in', position: 'absolute', right: '15rem', top: '3%' }}>Delete All</Button>
             <br></br>
-                <Table striped bordered hover size="sm">
-                    <thead>
-                        <tr>
-                            <th >S.N.</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Sulav</td>
-                            <td>Thapa</td>
-                            <td>@thapasulav21</td>
-                            <td><Button variant="success">Edit</Button></td>
-                            <td><Button variant="danger">Delete</Button></td>
-                        </tr>
-                    </tbody>
-                </Table>
+            <hr></hr>
+            <Table striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th >S.N.</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Sulav</td>
+                        <td>Thapa</td>
+                        <td>@thapasulav21</td>
+                        <td><Button variant="success">Edit</Button></td>
+                        <td><Button variant="danger">Delete</Button></td>
+                    </tr>
+                </tbody>
+            </Table>
+            {/* To deploy the simple pop in the project */}
+            {/* <Popup trigger={<button> Trigger</button>} position="center">
+                <div>Popup content here !!</div>
+            </Popup> */}
         </Card>
-    </div>
-        );
         
-        export default NewAdmin;
+    </div>
+);
+
+export default NewAdmin;
+
