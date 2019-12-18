13da268f891191ce88af259dca10162a5a35ceb8
sulav_21 <thapasulup@gmail.com>
2019-12-18 11:57:48 +0545

    To do list has been added using props, state and the components in the react app.

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -2,19 +2,51 @@ import React from 'react';
 import NavBar from '../Navbar';
 import Table from 'react-bootstrap/Table';
 import { Card, Button } from 'react-bootstrap';
-// import Fetchdata from '../FetchData/Fetch';
 import ModalMaker from './Modal/Modal';
+import SingleToDo from './SingleToDo';
 
-class NewAdmin extends React.Component{
+class NewAdmin extends React.Component {
 
-    render(){
-        return(
+    constructor(props) {
+        super(props);
+        this.state = {
+            todolist: [],
+            currentToDo: ""
+        };
+    }
+
+    onInputChange = e => {
+        this.setState({ currentToDo: e.target.value });
+    }
+
+    onClick = () => {
+        let toDoCopy = this.state.todolist.slice();
+        toDoCopy.push(this.state.currentToDo);
+
+        this.setState({ todolist: toDoCopy, currentToDo: "" })
+    }
+
+    deleteToDo = i => {
+        let toDoCopy = this.state.todolist.slice();
+        toDoCopy.splice(i ,1);
+        this.setState({todolist : toDoCopy});
+    }
+
+
+    render() {
+        let bulletToDo = this.state.todolist.map((e, i) => {
+            return (
+                <SingleToDo todo={e} delete={() => this.deleteToDo(i)}/>
+            );
+        });
+
+        return (
             <div>
                 <NavBar />
                 <br></br>
                 <Card className='mx-auto' bg="light" text="white" style={{ width: '80%', height: '100%', padding: 25 }}>
-                    <h3 style={{color:'black', top:'7%',position: 'absolute', left: '2rem'}}>Class 10 Students data</h3>
-                    <ModalMaker/>
+                    <h3 style={{ color: 'black', top: '7%', position: 'absolute', left: '2rem' }}>Class 10 Students data</h3>
+                    <ModalMaker />
                     <Button variant="primary" style={{ width: '1in', position: 'absolute', right: '8.5rem', top: '3%' }}>Import</Button>
                     <Button variant="danger" style={{ width: '1in', position: 'absolute', right: '15rem', top: '3%' }}>Delete All</Button>
                     <br></br>
@@ -46,8 +78,14 @@ class NewAdmin extends React.Component{
                         <div>Popup content here !!</div>
                     </Popup> */}
                 </Card>
-                
-            </div>
+                <br></br>
+                <input placeholder="Enter the to-do"
+                    value={this.state.currentToDo}
+                    onChange={this.onInputChange}></input>
+                <button onClick={this.onClick}>Add</button>
+                <br></br>
+                {this.state.todolist.length === 0 ? "No to-do on the list" : <ul>{bulletToDo}</ul>}
+            </div >
         );
     }
 }
---
+++ src/Components/SingleToDo.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Button} from 'react-bootstrap';
+
+class SingleToDo extends React.Component{
+  constructor(){
+    super();
+  }
+
+  render(){
+    return(
+      <div>
+        <li>{this.props.todo}
+        <Button variant="danger" onClick={this.props.delete}>Delete</Button></li>
+      </div>
+    );
+  }
+}
+
+export default SingleToDo;
