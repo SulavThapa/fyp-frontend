e4ad3690d090ac57370d643df904eb26ad2a5e1f
Sulav Thapa <bob@example.com>
2020-03-06 13:58:26 +0545

    Adding the login page authentication

--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwt_decode from "jwt-decode";
+import setAuthToken from "../utils/setAuthToken";
+
+import { setCurrentUser, logoutUser } from "../actions/authActions";
+import { Provider } from "react-redux";
+import store from "../store";
+
 import New from '../Components/Pages/Home';
 import NewAdmin from '../Components/Pages/Admin';
 import Error from '../Components/Pages/ErrorPage';
 import TableDesign from "../Components/Tables/Table";
 import BusInfo from "../Components/Pages/BusInfo";
+import PrivateRoute from "../Components/private-route/PrivateRoute";
+
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 class RoutePage extends React.Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Switch>
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Switch>
             <Route path="/" component={New} exact />
-            <Route path="/admin" component={NewAdmin} />
-            <Route path="/details" component={TableDesign} />
-            <Route path="/busDetails" component={BusInfo} />
-            <Route component={Error} />
-          </Switch>
-        </div>
-      </Router>
+              <Route exact path="/admin" component={NewAdmin} />
+              <Route exact path="/details" component={TableDesign} />
+              <Route exact path="/busDetails" component={BusInfo} />
+              <Route component={Error} />
+              <PrivateRoute exact path="/admin" component={NewAdmin} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
-import authReducers from "./authReducers";
-import errorReducers from "./errorReducers";
+import authReducer from "./authReducer";
+import errorReducer from "./errorReducer";
 
 export default combineReducers({
-  auth: authReducers,
-  errors: errorReducers
+  auth: authReducer,
+  errors: errorReducer
 });
